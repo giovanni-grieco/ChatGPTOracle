c7d8859d4df7ef36ba500ec9c4cc98d2f2247fca
Giovanni Grieco <giovi.5057@gmail.com>
2023-07-22 16:38:28 +0200

    app.properties aggiunto

--- src/main/java/it/uniroma3/chatGPT/Main.java
+++ src/main/java/it/uniroma3/chatGPT/Main.java
@@ -11,8 +11,10 @@ public class Main {
             ChatGPT gpt = new ChatGPT();
             List<GPTQuery> risposte;
             risposte = gpt.processPrompts(List.of("Is 5+5 equals to 10? answer with yes or no", "Can a squirrel fly? Answer with yes or no"), "text-davinci-003", 1000);
+            System.out.println("\n\nPrinting answers...");
+            System.out.println("----");
             for (GPTQuery risposta : risposte) {
-                System.out.println(risposta.toString());
+                System.out.println(risposta.toString()+"\n----");
             }
         }catch (IOException e){
             System.out.println(e.getMessage()+"\nAssicurarsi di avere un file chiamato app.properties nella root folder!");

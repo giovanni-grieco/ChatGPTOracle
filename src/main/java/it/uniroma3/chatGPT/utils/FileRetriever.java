e31609e7b49f26389a9d9af83b839f295675e9bf
Giovanni Grieco <giovi.5057@gmail.com>
2023-07-27 17:49:22 +0200

    Aggiunta eccezione in caso di file non trovato

--- src/main/java/it/uniroma3/chatGPT/utils/FileRetriever.java
+++ src/main/java/it/uniroma3/chatGPT/utils/FileRetriever.java
@@ -4,6 +4,10 @@ import java.io.File;
 
 public class FileRetriever {
     public static File getFile(String path) {
-        return new File(path);
+        File file = new File(path);
+        if (!file.exists()) {
+            throw new RuntimeException("File not found at '" + path + "'");
+        }
+        return file;
     }
 }

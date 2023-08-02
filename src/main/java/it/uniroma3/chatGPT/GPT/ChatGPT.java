44df26ff0e680ef0bfc82d7a37c2306130752a03
Giovanni Grieco <giovi.5057@gmail.com>
2023-08-02 12:27:19 +0200

    cambiata gestione delle eccezioni quando un prompt fallisce. Disaccoppiato ChatGPT da appProperties.

--- src/main/java/it/uniroma3/chatGPT/GPT/ChatGPT.java
+++ src/main/java/it/uniroma3/chatGPT/GPT/ChatGPT.java
@@ -27,22 +27,22 @@ public class ChatGPT {
     //private static String model= "text-babbage-001";
     private static String privateKey = null;
 
-    public ChatGPT() throws IOException {
-        privateKey = AppProperties.getAppProperties().getAPIKey();
+    public ChatGPT(String apiKey) throws IOException {
+        privateKey = apiKey;
     }
 
     public List<String> availableOpenAiModels() throws IOException {
         List<String> models = new ArrayList<>();
-        String myToken = "Bearer "+" "+privateKey;
+        String myToken = "Bearer " + " " + privateKey;
         HttpURLConnection conn = (HttpURLConnection) new URL(URL_AVAILABLE_MODELS).openConnection();
-        conn.setRequestProperty("Authorization",myToken);
-        conn.setRequestProperty("Content-Type","application/json");
+        conn.setRequestProperty("Authorization", myToken);
+        conn.setRequestProperty("Content-Type", "application/json");
         conn.setRequestMethod("GET");
         String output = new BufferedReader(new InputStreamReader(conn.getInputStream())).lines().reduce((a, b) -> a + b).get();
         JSONObject obj = new JSONObject(output);
         JSONArray dataArray = obj.getJSONArray("data");
 
-        for(Object JSONObj : dataArray){
+        for (Object JSONObj : dataArray) {
             JSONObject elem = (JSONObject) JSONObj;
             models.add(elem.get("id").toString());
         }
@@ -51,17 +51,17 @@ public class ChatGPT {
 
     public String answerQuestionCompletion(String text, String model) throws Exception {
 
-        String myToken = "Bearer "+" "+privateKey;
+        String myToken = "Bearer " + " " + privateKey;
         // E' necessario creare una connessione a risposta???? Non basta una connessione a per lista di prompt?
         HttpURLConnection conn = (HttpURLConnection) new URL(COMPLETION_URL_API).openConnection();
-        conn.setRequestProperty("Authorization",myToken);
-        conn.setRequestProperty("Content-Type","application/json");
+        conn.setRequestProperty("Authorization", myToken);
+        conn.setRequestProperty("Content-Type", "application/json");
         conn.setRequestMethod("GET");
 
         JSONObject data = new JSONObject();
         data.put("model", model);
         data.put("prompt", text);
-        data.put("max_tokens",700); // andrebbero diminuiti i max tokens, tanto deve dire solo si o no e rischieremmo di meno
+        data.put("max_tokens", 700); // andrebbero diminuiti i max tokens, tanto deve dire solo si o no e rischieremmo di meno
         data.put("temperature", 0.2);
         conn.setDoOutput(true);
         conn.getOutputStream().write(data.toString().getBytes());
@@ -91,65 +91,46 @@ public class ChatGPT {
         return new JSONObject(output).getJSONArray("choices").getJSONObject(0).getJSONObject("messages").getJSONObject("content").toString();
     }*/
 
-    public GPTQuery processPrompt(String prompt, String modelName){
+    public GPTQuery processPrompt(String prompt, String modelName) throws GPTException {
         try {
             String answer;
             System.out.println("Answering...");
             int initTime = (int) System.currentTimeMillis();
             answer = answerQuestionCompletion(prompt, modelName);
             int endTime = (int) System.currentTimeMillis();
-            return new GPTQuery(answer, modelName, prompt, endTime-initTime);
+            return new GPTQuery(answer, modelName, prompt, endTime - initTime);
         } catch (Exception e) {
-            return new GPTQuery("Exception raised->"+e.getMessage(), modelName, prompt, 0);
+            throw new GPTException(e.getMessage(), e.getCause());
         }
     }
 
     /**
      * Interroga il modello della OpenAI specificato fornendo una lista di prompt
-     * @param prompts Una lista d'interrogazioni testuali da fare al modello
-     * @param modelName modello da interrogare
+     *
+     * @param prompts     Una lista d'interrogazioni testuali da fare al modello
+     * @param modelName   modello da interrogare
      * @param millisDelay Delay fra una richiesta e l'altra
      * @return Una lista di risposte ottenute dal modello
      */
-    public List<GPTQuery> processPrompts(List<String> prompts, String modelName , int millisDelay) throws InterruptedException {
+    public List<GPTQuery> processPrompts(List<String> prompts, String modelName, int millisDelay) throws InterruptedException {
         List<GPTQuery> outputs = new ArrayList<>();
-        for(String prompt : prompts){
-            System.out.println("Asking: "+prompt);
-            GPTQuery answer = this.processPrompt(prompt, modelName);
-            outputs.add(answer);
-            System.out.println("Answer: "+answer.getRisposta());
-            System.out.println("Waiting "+millisDelay+"ms\n");
+        for (String prompt : prompts) {
+            System.out.println("Asking: " + prompt);
+            try {
+                GPTQuery answer = this.processPrompt(prompt, modelName);
+                outputs.add(answer);
+                System.out.println("Answer: " + answer.getRisposta());
+            } catch (GPTException e) {
+                e.printStackTrace();
+                System.out.println("Skipping to next prompt...");
+            }
+            System.out.println("Waiting " + millisDelay + "ms\n");
             Thread.sleep(millisDelay);
         }
         return outputs;
     }
+
     public static class PromptBuilder {
-        public static Iterable<String> generatePrompts(Iterable<Entity> entities) {
-            List<String> prompts = new ArrayList<>();
-            for(Entity firstEntity : entities) {
-                try {
-                    for (int i = 0; i < firstEntity.getData().size(); i++) {
-                        //l'unico di cui ho stabilito un template è ebay
-                        if (firstEntity.getData().get(i).getDomain().equals("www.ebay.com")) {
-                            String html = Files.readString(firstEntity.getData().get(i).toFullPath());
-                            String htmlFiltratoA = HTMLFilter.filterTemplate(html, HTMLFilter.DEFAULT_TAGS, firstEntity.getData().get(i).getDomain());
-                            for (int j = i + 1; j < firstEntity.getData().size(); j++) {
-                                if (firstEntity.getData().get(j).getDomain().equals("www.ebay.com")) {
-                                    String html1 = Files.readString(firstEntity.getData().get(j).toFullPath());
-                                    String htmlFiltratoB = HTMLFilter.filterTemplate(html1, HTMLFilter.DEFAULT_TAGS, firstEntity.getData().get(j).getDomain());
-                                    prompts.add(buildPromptTwoSnippets(htmlFiltratoA, htmlFiltratoB));
-                                }
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    System.out.println(e.getMessage());
-                    System.out.println(firstEntity.toString());
-                }
-            }
-            return prompts;
-        }
 
         public static String buildPromptTwoSnippets(String webPageA, String webPageB) {
             String prompt = "You will be given 2 snippets of text talking about an entity, object or attribute.\n";

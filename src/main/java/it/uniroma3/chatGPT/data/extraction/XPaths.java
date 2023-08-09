933a0579ed0ddf5cc2dbc5b126f88262b06082fd
Giovanni Grieco <giovi.5057@gmail.com>
2023-08-10 00:17:56 +0200

    cambiate variabili a final

--- src/main/java/it/uniroma3/chatGPT/data/extraction/XPaths.java
+++ src/main/java/it/uniroma3/chatGPT/data/extraction/XPaths.java
@@ -3,26 +3,11 @@ package it.uniroma3.chatGPT.data.extraction;
 import java.util.List;
 
 public class XPaths {
-
-    private String templateName;
-
-    private List<String> xPaths;
-
+    private final String templateName;
+    private final List<String> xPaths;
 
     public XPaths(String templateName, List<String> xPaths) {
         this.templateName = templateName;
-        this.xPaths = xPaths;
-    }
-
-    public void setTemplateName(String templateName) {
-        this.templateName = templateName;
-    }
-
-    public String getTemplateName() {
-        return templateName;
-    }
-
-    public void setXPaths(List<String> xPaths) {
         this.xPaths = xPaths;
     }
 

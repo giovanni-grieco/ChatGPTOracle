17a727fda4d6b3c9c1d7fc3c3fe2aef75bd7aefd
Giovanni Grieco <giovi.5057@gmail.com>
2023-08-10 00:18:06 +0200

    niente

--- src/main/java/it/uniroma3/chatGPT/data/extraction/HTMLTemplateException.java
+++ src/main/java/it/uniroma3/chatGPT/data/extraction/HTMLTemplateException.java
@@ -4,7 +4,6 @@ public class HTMLTemplateException extends Exception{
     public HTMLTemplateException(String message, Throwable cause) {
         super(message, cause);
     }
-
     public HTMLTemplateException(String message) {
         super(message);
     }

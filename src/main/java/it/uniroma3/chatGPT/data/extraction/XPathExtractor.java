877e6fcddc573c44739fc68ae244b1096adc8424
Giovanni Grieco <giovi.5057@gmail.com>
2023-08-09 11:29:58 +0200

    implementata classe che estrae da una pagina html le informazioni tramite XPath

---
+++ src/main/java/it/uniroma3/chatGPT/data/extraction/XPathExtractor.java
@@ -0,0 +1,47 @@
+package it.uniroma3.chatGPT.data.extraction;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import java.io.StringReader;
+
+public class XPathExtractor {
+
+    private final String xpath;
+    private String xml;
+
+    public XPathExtractor(String xpath, String xml) {
+        this.xpath = xpath;
+        this.xml = xml;
+    }
+
+    public String extract() {
+        try {
+            this.xml = xml.replace("&nbsp;", "&#160;");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setValidating(false);
+            factory.setIgnoringElementContentWhitespace(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            // Parse the XML string into a Document object
+            Document document = builder.parse(new InputSource(new StringReader(this.xml)));
+            // Create an XPathFactory
+            XPathFactory xPathFactory = XPathFactory.newInstance();
+            XPath xPath = xPathFactory.newXPath();
+            // Compile the XPath expression
+            XPathExpression expression = xPath.compile(this.xpath);
+            // Evaluate the expression and get the result
+            // Print the extracted text
+            return (String) expression.evaluate(document, XPathConstants.STRING);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}

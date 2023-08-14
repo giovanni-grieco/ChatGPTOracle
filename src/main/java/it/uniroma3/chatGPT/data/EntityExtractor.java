ab7ddd5ddc54ffca4438814437524a338ebc5778
Giovanni Grieco <giovi.5057@gmail.com>
2023-08-14 19:58:46 +0200

    aggiunto attributo "type" per descrivere una entità proveniente da diversi dataset.

--- src/main/java/it/uniroma3/chatGPT/data/EntityExtractor.java
+++ src/main/java/it/uniroma3/chatGPT/data/EntityExtractor.java
@@ -1,10 +1,8 @@
 package it.uniroma3.chatGPT.data;
 
-import it.uniroma3.chatGPT.AppProperties;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -12,18 +10,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class EntityExtractor {
+
+    private final int typeOfEntityToBeExtracted;
     private final Path groundTruthPath;
 
-    public EntityExtractor(Path path) {
-        groundTruthPath = path;
+    public EntityExtractor(int typeOfEntityToBeExtracted, Path path) {
+        this.typeOfEntityToBeExtracted = typeOfEntityToBeExtracted;
+        this.groundTruthPath = path;
     }
 
     public Set<Entity> extractEntitiesFromGroundTruth() throws IOException {
         Set<Entity> entities = new HashSet<>();
+        return this.extractEntitiesFromGroundTruth(entities);
+    }
+
+    public Set<Entity> extractEntitiesFromGroundTruth(Set<Entity> entities) throws IOException {
         FileReader reader = new FileReader(groundTruthPath.toString());
         Iterable<CSVRecord> records = CSVFormat.Builder.create().setHeader().setSkipHeaderRecord(true).build().parse(reader);
         for (CSVRecord record : records) {
-            Entity newEntity = new Entity(record.get(0));
+            Entity newEntity = new Entity(this.typeOfEntityToBeExtracted, record.get(0));
             if (entities.contains(newEntity)) {
                 entities.stream().filter(e -> e.equals(newEntity)).findAny().orElseThrow().addHtmlLocation(record.get(1));
             } else {

31c55c4598641a98cee8fb29d24bab28e70f76c8
Giovanni Grieco <giovi.5057@gmail.com>
2023-08-19 12:47:30 +0200

    creata classe dedicata alla preparazione del dataset da usare per fare finetuning

---
+++ src/main/java/it/uniroma3/chatGPT/GPT/fineTuning/JSONLineGenerator.java
@@ -0,0 +1,22 @@
+package it.uniroma3.chatGPT.GPT.fineTuning;
+
+import java.util.List;
+
+public class JSONLineGenerator {
+    public static String generateJSONLines(List<String> prompts){
+        StringBuilder sb = new StringBuilder();
+        int iterator = 0;
+        for (String p : prompts) {
+            sb.append("{");
+            sb.append("\"prompt\":\"").append(p).append("\",");
+            if (iterator <= prompts.size() / 2) {
+                sb.append(" \"completion\":\"no\"");
+            } else {
+                sb.append("\"completion\":\" yes\"");
+            }
+            sb.append("}\n");
+            iterator++;
+        }
+        return sb.toString();
+    }
+}

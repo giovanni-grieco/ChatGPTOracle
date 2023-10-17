23f525c5a6c30827231a5861930a49868978834a
Giovanni Grieco <giovi.5057@gmail.com>
2023-10-17 08:50:15 +0200

    Finito blocking

--- src/main/java/it/uniroma3/chatGPT/comando/comandi/BloccoCanon.java
+++ src/main/java/it/uniroma3/chatGPT/comando/comandi/BloccoCanon.java
@@ -3,28 +3,15 @@ package it.uniroma3.chatGPT.comando.comandi;
 import it.uniroma3.chatGPT.Application;
 import it.uniroma3.chatGPT.comando.AnalisiCompletaWorkerThread;
 import it.uniroma3.chatGPT.comando.Comando;
-import it.uniroma3.chatGPT.comando.BloccoCanonWorkerThread;
 import it.uniroma3.chatGPT.data.Data;
 import it.uniroma3.chatGPT.data.Entity;
-import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.IOException;
 import java.util.*;
 
 public class BloccoCanon implements Comando {
 
-    //Implementato per motivi di ottimizzazione
-
-
-    public static void putPairInMap(Map<Entity, List<Pair<Data, Data>>> entity2PairOfData, Entity e, Pair<Data, Data> pairOfData) {
-        if (entity2PairOfData.containsKey(e)) {
-            entity2PairOfData.get(e).add(pairOfData);
-        } else {
-            List<Pair<Data, Data>> pairOfDataList = new ArrayList<>();
-            pairOfDataList.add(pairOfData);
-            entity2PairOfData.put(e, pairOfDataList);
-        }
-    }
+    private static final String[] blocksDiscriminants = {"Canon", "Nikon", ""};
 
     @Override
     public void esegui(Application application) throws InterruptedException, IOException {
@@ -40,51 +27,49 @@ public class BloccoCanon implements Comando {
             }
         }
 
-        //loadData2TitleMap(entityList);
+        //filtriamo tutte le entità che hanno nel campo title il discriminante del blocco che vogliamo analizzare
+        for (String blockDiscriminant : blocksDiscriminants) {
+            List<Entity> inBlockEntities = new ArrayList<>();
+            for (Entity e : entityList) {
+                for (Data d : e.getData()) {
+                    if (belongToBlock(d.getTitle(), blockDiscriminant)) {
+                        inBlockEntities.add(e);
+                        break;
+                    }
+                }
+            }
 
-        Map<Entity, List<Pair<Data, Data>>> entity2PairOfData = new HashMap<>();
+            //Stampa di prova
+            for (Entity e : inBlockEntities) {
+                System.out.println(e.getName());
+                for (Data d : e.getData()) {
+                    System.out.println(d.getTitle());
+                }
+            }
 
-        int numThread = Runtime.getRuntime().availableProcessors();
-        Thread[] workers = new Thread[numThread];
-        for (int i = 0; i < numThread; i++) {
-            System.out.println("Thread " + i + " started");
-            workers[i] = new BloccoCanonWorkerThread(i, entityList.subList(i * entityList.size() / numThread, (i + 1) * entityList.size() / numThread), entity2PairOfData);
-            System.out.println(entityList.subList(i * entityList.size() / numThread, (i + 1) * entityList.size() / numThread));
-            workers[i].start();
-        }
+            int numeroDiPromptTotali = 2500;
+            int percentualePositivi = 0;
 
-        for (int i = 0; i < numThread; i++) {
-            workers[i].join();
-        }
-        System.out.println("All threads finished");
-        System.out.println("Data Cache Dump");
-        System.out.println(Data.Cache.dumpCacheInfo());
+            List<Thread> workerThreads = new ArrayList<>();
+            while (percentualePositivi != 100) {
+                System.out.println("Creazione thread");
+                Thread t = new AnalisiCompletaWorkerThread(application, 0, percentualePositivi, numeroDiPromptTotali, inBlockEntities);
+                workerThreads.add(t);
+                percentualePositivi += 5;
+                t.start();
+            }
 
-        List<Entity> allCanons = new ArrayList<>(entity2PairOfData.keySet());
-        for (Entity e : allCanons) {
-            System.out.println(e.getName());
-            for (Data d : e.getData()) {
-                System.out.println(d.getTitle());
+            for (Thread t : workerThreads) {
+                t.join();
             }
         }
 
-        int numeroDiPromptTotali = 2000;
-        int percentualePositivi = 0;
-
-        List<Thread> workerThreads = new ArrayList<>();
-        while (percentualePositivi != 100) {
-            System.out.println("Creazione thread");
-            workerThreads.add(new AnalisiCompletaWorkerThread(application, 0, percentualePositivi, numeroDiPromptTotali, allCanons));
-            percentualePositivi += 5;
-        }
-
-        for (Thread t : workerThreads) {
-            t.start();
-            t.join();
-        }
-        System.out.println("Analisi completa finita. Thread arrestati.");
-
-
-        //System.out.println(entity2PairOfData);
     }
+
+    private boolean belongToBlock(String text, String blockDiscriminant) {
+        String textLower = text.toLowerCase();
+        String blockDiscriminantLower = blockDiscriminant.toLowerCase();
+        return textLower.contains(blockDiscriminantLower);
+    }
+
 }

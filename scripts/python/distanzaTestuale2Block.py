79c2c33541fe22b5e4366750630788243f27f59c
Giovanni Grieco <giovi.5057@gmail.com>
2023-11-06 10:38:49 +0100

    implementato script per la manipolazione di csv

--- scripts/python/distanzaTestuale2Block.py
+++ scripts/python/distanzaTestuale2Block.py
@@ -1,6 +1,23 @@
 from pathlib import Path
 import os
 import pandas as pd
+import re
+from Levenshtein import distance
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Definisci i due testi
+
+
+stringa1 = "casa"
+stringa2 = "cassa"
+
+# Calcola la distanza di Levenshtein
+distanza = distance(stringa1, stringa2)
+
+# Stampa la distanza
+print(f"La distanza di Levenshtein tra '{stringa1}' e '{stringa2}' è {distanza}.")
+
 
 def main():
     folder_path = Path(os.path.abspath(__file__))  # Replace with your folder path
@@ -20,16 +37,47 @@ def main():
     fileIndex = int(input())
     fileSelected = files[fileIndex]
     print("You have selected file " + str(fileIndex) + ": " + str(fileSelected))
-    spreadsheet = pd.read_excel(fileSelected)
+    spreadsheet = pd.read_excel(io=fileSelected)
     columnName = "Blocco"
     column_elements = spreadsheet[columnName]
-    for i in range(1, len(column_elements)):
-        block_name = column_elements[i]
-        print(block_name)
-        block_sheet = pd.read_excel(fileSelected, sheet_name=block_name) #FIXARE
+    for i in range(0, len(column_elements)):
+        try:
+            block_name = column_elements[i]
+            sheetName = "Blocco "+block_name
+            block_sheet = pd.read_excel(io=str(fileSelected), sheet_name=str(sheetName)) #FIXARE
+            prima_colonna = block_sheet.iloc[:, 0].tolist()
+            promptAmount = len(prima_colonna)
+            distanzaAccumulata = 0
+            for i in range (0, len(prima_colonna)):
+                risultato = splitText(prima_colonna[i], "second: ")
+                text1 = risultato[0].lower()
+                text2 = risultato[1].lower()
+                text1 = removeFromText(text1, "first: ")
+                distanza = levenshteinDistance(text1, text2)#distance(text1, text2)
+                #print(f"La distanza di Levenshtein tra '{text1}' e '{text2}' è {distanza}.")
+                distanzaAccumulata = distanzaAccumulata + distanza
+            distanzaMedia = distanzaAccumulata / promptAmount
+            print(f"{distanzaMedia}")
+        except Exception as e:
+            print(e)
 
+def splitText(text, splitter):
+    return re.split(splitter, text)
+def removeFromText(text, toRemove):
+    return text.replace(toRemove, "")
 
+def cosineDistance(text1, text2):
+    testo1 = text1
+    testo2 = text2
+    # Inizializza il vettorizzatore TF-IDF
+    tfidf_vectorizer = TfidfVectorizer()
+    # Crea la matrice TF-IDF
+    matrice_tfidf = tfidf_vectorizer.fit_transform([testo1, testo2])
+    # Calcola la similarità coseno
+    return (cosine_similarity(matrice_tfidf[0], matrice_tfidf[1]))[0][0]
 
+def levenshteinDistance(text1, text2):
+    return distance(text1, text2)
 
 if __name__ == '__main__':
     main()

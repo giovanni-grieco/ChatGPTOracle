8b1cb1daa3cd6364c76bba64165a34ecc0a39a37
Giovanni Grieco <giovi.5057@gmail.com>
2023-11-22 15:08:12 +0100

    Introdotte feature di calcolo del tempo rimasto stimato.

--- src/main/java/it/uniroma3/LLMOracle/GPT/LLM.java
+++ src/main/java/it/uniroma3/LLMOracle/GPT/LLM.java
@@ -16,6 +16,13 @@ public abstract class LLM {
     //private static String model= "text-babbage-001";
     protected static String privateKey = null;
 
+    private final List<Integer> timeList = new ArrayList<>();
+
+    private final List<Character> loadingChars = List.of('|', '/', '-', '\\');
+
+    int loadingCharIndex = 0;
+
+
     protected LLM(String apiKey) {
         LLM.privateKey = apiKey;
     }
@@ -24,28 +31,43 @@ public abstract class LLM {
 
     public List<GPTQuery> processPrompts(List<Prompt> prompts, String modelName, int millisDelay) throws InterruptedException{
         List<GPTQuery> outputs = new ArrayList<>();
+        int processedPrompts=0;
+        int totalPrompts = prompts.size();
+        boolean firstPrompt = true;
         for (Prompt prompt : prompts) {
-            System.out.println("Asking: " + prompt.toString());
+            //System.out.println("Asking: " + prompt.toString());
             int querytime = 0;
             try {
                 GPTQuery answer = this.processPrompt(prompt, modelName);
                 outputs.add(answer);
-                System.out.println("Answer: " + answer.getRisposta());
+                processedPrompts++;
+                //System.out.println("Answer: " + answer.getRisposta());
                 querytime = answer.getTempoDiRisposta();
+                this.addTime(querytime);
             } catch (GPTException e) {
                 System.out.println("Error: " + e.getMessage());
                 System.out.println("Skipping to next prompt...");
             }
-            System.out.println("Waited " + (millisDelay+querytime) + "ms\n");
-            //Thread.sleep(millisDelay);
+            String estimatedTime = String.format("%.2f", ((this.getAverageTime()*(prompts.size()-processedPrompts)))/1000);
+            String output = "Processed prompts: "+processedPrompts+"/"+totalPrompts+" "+"estimated time remaining "+estimatedTime+"s "+this.getNextLoadingChar();
+            int outputLength = output.length();
+            if(!firstPrompt){
+                /*for(int i = 0; i<outputLength; i++){
+                    System.out.print("\b");
+                }*/
+                System.out.print("\r");
+            }
+            firstPrompt = false;
+            System.out.print(output);
         }
+        System.out.println();
         return outputs;
     }
 
     public GPTQuery processPrompt(Prompt prompt, String modelName) throws GPTException {
         try {
             String answer;
-            System.out.println("Answering...");
+            //System.out.println("Answering...");
             int initTime = (int) System.currentTimeMillis();
             answer = answerQuestionCompletion(prompt.getTextPrompt(), modelName);
             int endTime = (int) System.currentTimeMillis();
@@ -53,5 +75,31 @@ public abstract class LLM {
         } catch (Exception e) {
             throw new GPTException(e.getMessage(), e.getCause());
         }
+    }
+
+    private char getNextLoadingChar(){
+        if(loadingCharIndex>=this.loadingChars.size()){
+            loadingCharIndex = 0;
+        }
+        char nextLoadingChar = this.loadingChars.get(loadingCharIndex);
+        loadingCharIndex++;
+        return nextLoadingChar;
+    }
+
+    private void addTime(int time){
+        if(this.timeList.size()<=10){
+            this.timeList.add(time);
+        }else{
+            this.timeList.remove(0);
+            this.timeList.add(time);
+        }
+    }
+
+    private float getAverageTime(){
+        int sum = 0;
+        for(int i : this.timeList){
+            sum+=i;
+        }
+        return (float)sum/this.timeList.size();
     }
 }
